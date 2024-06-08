e25aa1de58d7d9013e7952d0edf773779fe1dc50
tresor kasenda <tresorkasendat@gmail.com>
2024-06-08 18:19:50 +0200

    [ui] add badge

---
+++ resources/js/Components/tailus-ui/badge/Badge.tsx
@@ -0,0 +1,22 @@
+import {badge, type BadgeProps} from "@tailus/themer"
+
+export interface BadgeVariantsProps extends React.HTMLAttributes<HTMLDivElement>, BadgeProps {
+}
+
+export const Badge: React.FC<BadgeVariantsProps> = ({
+                                                        className,
+                                                        children,
+                                                        intent = "primary",
+                                                        size = "sm",
+                                                        variant = "soft",
+                                                        ...props
+                                                    }) => {
+    return (
+        <span className={badge[variant]({intent, size, className})} {...props}>
+            {children}
+        </span>
+    )
+}
+
+Badge.displayName = "Badge";
+export default Badge;
